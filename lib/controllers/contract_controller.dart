b04d18226c4f2945c9ef2d0c5115571c3462a4b3
Yazan420 <yazanmahfooz8@gmail.com>
2025-12-26 23:24:15 +0300

    ..

--- lib/controllers/contract_controller.dart
+++ lib/controllers/contract_controller.dart
@@ -10,17 +10,6 @@ class ContractController {
   Future<List<Contracts>> loadContractsHistory(int page) async {
     try {
       final List<Contracts> contracts =
-          await contractService.showContractsHistory(page) ?? [];
-
-      _contracts = contracts;
-      return _contracts;
-    } catch (e) {
-      rethrow;
-    }
-  }
-  Future<List<Contracts>> loadContractsScreen(int page) async {
-    try {
-      final List<Contracts> contracts =
           await contractService.showContractsScreen(page) ?? [];
 
       _contracts = contracts;
@@ -29,6 +18,19 @@ class ContractController {
       rethrow;
     }
   }
+
+  Future<List<Contracts>> loadContractsScreen(int page) async {
+    try {
+      final List<Contracts> contracts =
+          await contractService.showContractsHistory(page) ?? [];
+
+      _contracts = contracts;
+      return _contracts;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
   Future<Contracts?> bookApartment({
     required int apartmentId,
     required DateTime start,
@@ -48,8 +50,7 @@ class ContractController {
     required DateTime start,
     required DateTime end,
   }) async {
-    final Contracts? updatedRent =
-        await contractService.updateContract(
+    final Contracts? updatedRent = await contractService.updateContract(
       rentId: rentId,
       start: start,
       end: end,
@@ -68,8 +69,9 @@ class ContractController {
   }
 
   Future<Contracts> approveContract({required int rentId}) async {
-    final Contracts approvedRent =
-        await contractService.approveRent(rentId: rentId);
+    final Contracts approvedRent = await contractService.approveRent(
+      rentId: rentId,
+    );
 
     final index = _contracts.indexWhere((c) => c.id == rentId);
     if (index != -1) {
@@ -80,21 +82,18 @@ class ContractController {
   }
 
   Future<Contracts> rejectContract({required int rentId}) async {
-      final rejectedContract =
-          await contractService.rejectRent(rentId: rentId);
+    final rejectedContract = await contractService.rejectRent(rentId: rentId);
 
-      return rejectedContract;
-    }
-
+    return rejectedContract;
+  }
 
   Future<bool> cancelRent({required int rentId}) async {
-      final success = await contractService.deleteContract(rentId: rentId);
+    final success = await contractService.deleteContract(rentId: rentId);
 
-      if (success) {
-        _contracts.removeWhere((c) => c.id == rentId);
-      }
+    if (success) {
+      _contracts.removeWhere((c) => c.id == rentId);
+    }
 
-      return success;
-
+    return success;
   }
 }
