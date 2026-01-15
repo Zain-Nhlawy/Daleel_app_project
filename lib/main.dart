37ff228252dd301faddf6e8b87aaa4912bf1356d
mmmmohammadalhomsi@gmail.com <mmmmohammadalhomsi@gamil.com>
2026-01-15 09:54:20 +0300

    fix controller bug

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:daleel_app_project/models/user.dart';
 import 'package:daleel_app_project/providers.dart';
 import 'package:daleel_app_project/screen/home_screen/notifications_screen.dart';
 import 'package:daleel_app_project/screen/splash/splash_screen.dart';
+import 'package:daleel_app_project/screen/tabs_screen/home_screen_tabs.dart';
 import 'package:provider/provider.dart';
 import 'package:daleel_app_project/services/firebase_notification_service.dart';
 
@@ -160,7 +161,7 @@ class MyApp extends StatelessWidget {
       theme: theme,
       darkTheme: darkTheme,
       themeMode: provider.currentTheme,
-      home: SplashScreen(),
+      home: HomeScreenTabs(),
       locale: provider.currentLocale,
       supportedLocales: const [Locale('en'), Locale('ar'), Locale('fr')],
       localizationsDelegates: const [
